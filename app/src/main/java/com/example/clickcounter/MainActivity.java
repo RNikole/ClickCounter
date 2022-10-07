bb8ccfdbde9b2a4431fa85d2bf43cbbc9f9d5705
Niki <Niki@NIKI-PC>
2022-10-07 17:56:59 +0200

    elemek hozzáadva

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.clickcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private RelativeLayout relativeLayout;
+    private Button buttonPlus, buttonMinus;
+    private TextView textviewCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void init() {
         relativeLayout = findViewById(R.id.relativeLayout);
+        buttonPlus = findViewById(R.id.buttonPlus);
+        buttonMinus = findViewById(R.id.buttonMinus);
+        textviewCounter = findViewById(R.id.textviewCounter);
+    }
+
+    public void PlusClick(View view) {
+
+    }
+
+    public void MinusClick(View view) {
+
+    }
+
+    public void CounterClick(View view) {
+
     }
 }
